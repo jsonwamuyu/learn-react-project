df39394b886ed6afbf24cb1c473d07f438cdeeef
jsonwamuyu <jysonmuchiri@gmail.com>
2025-02-19 13:06:14 +0300

    Add github workflows

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import "./App.css";
-import ProductCategory from "./components/ProductCategory";
+import { FilterableProductTable } from "./components/FilterableProductTable";
 import { myProductsData } from "./utils";
 
 function App() {
-  // const [count, setCount] = useState(0)
-  
-
   return (
     <div className="max-w-[900px] py-[1rem] px-[2rem] mx-auto my-[0] bg-blue-50 min-h-screen w-full">
       <div className="flex flex-row justify-center items-center">
-        <h1 className="text-blue-600 text-5xl">Welcome</h1>
-        <p className="max-w-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-          voluptatum.
-        </p>
+        <FilterableProductTable product={myProductsData} />
       </div>
-      {/* loop through the product data */}
-      {myProductsData.map((product, index) => {
-        return <ProductCategory key={index} category={product.category} />;
-      })}
     </div>
   );
 }
